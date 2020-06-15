a0f78a9b96ecfd16e80745665acc4db3c7bbb1fb
Mitsuki <mooka0907@gmail.com>
2020-06-15 10:59:35 -0400

    import design template

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery/jquery.min
+//= require bootstrap/js/bootstrap.bundle.min
+//= require jquery.easing/jquery.easing.min
+//= require php-email-form/validate
+//= require waypoints/jquery.waypoints.min
+//= require counterup/counterup.min
+//= require owl.carousel/owl.carousel.min
+//= require isotope-layout/isotope.pkgd.min
+//= require venobox/venobox.min
+//= require main
