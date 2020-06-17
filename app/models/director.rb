507e45c2e09b319f213a850411aaf671b64f46a4
Mitsuki <mooka0907@gmail.com>
2020-06-16 23:16:30 -0400

    Director error

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,7 +1,10 @@
 class Director < ApplicationRecord
+  mount_uploader :image, WorkImageUploader
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :from, presence: true
+
+  acts_as_list
 
   has_many :works
 end
