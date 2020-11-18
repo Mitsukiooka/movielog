bd8cf013f5901e947de561a4aba48829bbf7d676
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2020-11-18 09:15:53 +0900

    secret

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => "#{Rails.application.credentials.sendgrid_apikey}",
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'apikey',
+#   :password => "#{Rails.application.credentials.sendgrid_apikey}",
+#   :domain => 'yourdomain.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
